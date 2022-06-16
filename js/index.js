7ec046c9ed4fb5ec1498857e27d3d3a209870f1f
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-06-16 19:05:20 +0900

    220616
    section_cha edit.

--- js/index.js
+++ js/index.js
@@ -1,28 +1,37 @@
-$(function(){
-    $(".menu").mouseover(function(){
-        $(".sub_menu").css("display", "block");
-    });
-    $(".menu").mouseout(function(){
-        $(".sub_menu").css("display", "none");
+$(function () {
+    $(".menu>li").mouseover(function () {
+        $(".sub_menu").stop().slideDown();
+    }).mouseout(function () {
+        $(".sub_menu").stop().slideUp();
     });
 });
 
-$(function(){
-    $(".slide_btn1").click(function(){
+$(function () {
+    $(".slide_btn1").click(function () {
         $(".slide_box1").css("display", "block");
         $(".slide_box2").css("display", "none");
         $(".slide_box3").css("display", "none");
+        $(".slide_btn1").css("background-color", "white");
+        $(".slide_btn2").css("background-color", "gray");
+        $(".slide_btn3").css("background-color", "gray");
     });
 
-    $(".slide_btn2").click(function(){
+    $(".slide_btn2").click(function () {
         $(".slide_box1").css("display", "none");
         $(".slide_box2").css("display", "block");
         $(".slide_box3").css("display", "none");
+        $(".slide_btn2").css("background-color", "white");
+        $(".slide_btn1").css("background-color", "gray");
+        $(".slide_btn3").css("background-color", "gray");
     });
 
-    $(".slide_btn3").click(function(){
+    $(".slide_btn3").click(function () {
         $(".slide_box1").css("display", "none");
         $(".slide_box2").css("display", "none");
         $(".slide_box3").css("display", "block");
+        $(".slide_btn3").css("background-color", "white");
+        $(".slide_btn1").css("background-color", "gray");
+        $(".slide_btn2").css("background-color", "gray");
     });
 });
+
