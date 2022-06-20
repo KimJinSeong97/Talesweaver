b7d66c25f4438285497ad68fd23d70a76b63d61e
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-06-20 09:33:50 +0900

    220619
    0618-0619 img edit.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// menu hover submenu on/off
 $(function () {
     $(".menu>li").mouseover(function () {
         $(".sub_menu").stop().slideDown();
@@ -6,6 +7,7 @@ $(function () {
     });
 });
 
+// slide btn click next slide
 $(function () {
     $(".slide_btn1").click(function () {
         $(".slide_box1").css("display", "block");
@@ -35,3 +37,37 @@ $(function () {
     });
 });
 
+// character hover and click explain
+
+
+
+
+// skill icon click explain
+$(function () {
+    $(".cha_right .ability img:first-of-type").click(function () {
+        $(".skill_text1").css("display", "block");
+        $(".skill_text2").css("display", "none");
+        $(".skill_text3").css("display", "none");
+        $(this).css("opacity", 1);
+        $(".cha_right .ability img:nth-of-type(2)").css("opacity", 0.5);
+        $(".cha_right .ability img:nth-of-type(3)").css("opacity", 0.5);
+    });
+
+    $(".cha_right .ability img:nth-of-type(2)").click(function () {
+        $(".skill_text2").css("display", "block");
+        $(".skill_text1").css("display", "none");
+        $(".skill_text3").css("display", "none");
+        $(this).css("opacity", 1)
+        $(".cha_right .ability img:first-of-type").css("opacity", 0.5);
+        $(".cha_right .ability img:nth-of-type(3)").css("opacity", 0.5);
+    });
+
+    $(".cha_right .ability img:nth-of-type(3)").click(function () {
+        $(".skill_text3").css("display", "block");
+        $(".skill_text1").css("display", "none");
+        $(".skill_text2").css("display", "none");
+        $(this).css("opacity", 1)
+        $(".cha_right .ability img:first-of-type").css("opacity", 0.5);
+        $(".cha_right .ability img:nth-of-type(2)").css("opacity", 0.5);
+    });
+});
