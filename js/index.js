56d5f6b2195131671401c599d0f064eda0fae789
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-06-20 18:21:00 +0900

    220620
    worldview start.

--- js/index.js
+++ js/index.js
@@ -7,40 +7,72 @@ $(function () {
     });
 });
 
+// auto slide
+let i = 0;
+
+function slide(){
+    i++;
+
+    if(i>2){
+        i = 0;
+    }
+    $(".slide").eq(i).stop().show();
+    $(".slide").eq(i-1).stop().hide();
+}
+
+setInterval(slide, 5000);
+
 // slide btn click next slide
 $(function () {
     $(".slide_btn1").click(function () {
         $(".slide_box1").css("display", "block");
         $(".slide_box2").css("display", "none");
         $(".slide_box3").css("display", "none");
-        $(".slide_btn1").css("background-color", "white");
-        $(".slide_btn2").css("background-color", "gray");
-        $(".slide_btn3").css("background-color", "gray");
     });
 
     $(".slide_btn2").click(function () {
         $(".slide_box1").css("display", "none");
         $(".slide_box2").css("display", "block");
         $(".slide_box3").css("display", "none");
-        $(".slide_btn2").css("background-color", "white");
-        $(".slide_btn1").css("background-color", "gray");
-        $(".slide_btn3").css("background-color", "gray");
     });
 
     $(".slide_btn3").click(function () {
         $(".slide_box1").css("display", "none");
         $(".slide_box2").css("display", "none");
         $(".slide_box3").css("display", "block");
-        $(".slide_btn3").css("background-color", "white");
-        $(".slide_btn1").css("background-color", "gray");
-        $(".slide_btn2").css("background-color", "gray");
     });
 });
 
-// character hover and click explain
+// character click detail info
+$(function(){
+    $(".cha1_back").click(function(){
+        $(".cha_info1").css("display", "block");
+        $(".cha_info2").css("display", "none");
+        $(".cha_info3").css("display", "none");
+        $(".cha_info4").css("display", "none");
+    });
 
+    $(".cha2_back").click(function(){
+        $(".cha_info1").css("display", "none");
+        $(".cha_info2").css("display", "block");
+        $(".cha_info3").css("display", "none");
+        $(".cha_info4").css("display", "none");
+    });
 
+    $(".cha3_back").click(function(){
+        $(".cha_info1").css("display", "none");
+        $(".cha_info2").css("display", "none");
+        $(".cha_info3").css("display", "block");
+        $(".cha_info4").css("display", "none");
+    });
 
+    $(".cha4_back").click(function(){
+        $(".cha_info1").css("display", "none");
+        $(".cha_info2").css("display", "none");
+        $(".cha_info3").css("display", "none");
+        $(".cha_info4").css("display", "block");
+    });
+});
 
 // skill icon click explain
 $(function () {
@@ -48,7 +80,7 @@ $(function () {
         $(".skill_text1").css("display", "block");
         $(".skill_text2").css("display", "none");
         $(".skill_text3").css("display", "none");
-        $(this).css("opacity", 1);
+        $(".cha_right .ability img:first-of-type").css("opacity", 1);
         $(".cha_right .ability img:nth-of-type(2)").css("opacity", 0.5);
         $(".cha_right .ability img:nth-of-type(3)").css("opacity", 0.5);
     });
@@ -57,7 +89,7 @@ $(function () {
         $(".skill_text2").css("display", "block");
         $(".skill_text1").css("display", "none");
         $(".skill_text3").css("display", "none");
-        $(this).css("opacity", 1)
+        $(".cha_right .ability img:nth-of-type(2)").css("opacity", 1)
         $(".cha_right .ability img:first-of-type").css("opacity", 0.5);
         $(".cha_right .ability img:nth-of-type(3)").css("opacity", 0.5);
     });
@@ -66,8 +98,11 @@ $(function () {
         $(".skill_text3").css("display", "block");
         $(".skill_text1").css("display", "none");
         $(".skill_text2").css("display", "none");
-        $(this).css("opacity", 1)
+        $(".cha_right .ability img:nth-of-type(3)").css("opacity", 1)
         $(".cha_right .ability img:first-of-type").css("opacity", 0.5);
         $(".cha_right .ability img:nth-of-type(2)").css("opacity", 0.5);
     });
 });
+
+
+
